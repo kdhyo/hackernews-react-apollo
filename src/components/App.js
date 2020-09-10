707826630ef82479e6ec36dfbea112659dff1dad
hyo <kdhyo98@gmail.com>
2020-09-10 17:27:30 +0900

    Queries: Loading Links

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.css";
+import LinkList from "./LinkList";
+
+function App() {
+  return (
+    <div className="App">
+      <LinkList />
+    </div>
+  );
+}
+
+export default App;
